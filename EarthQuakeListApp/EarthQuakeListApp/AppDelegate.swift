75a986c368635586d52d8499d2f39bab9a519a4b
Jagaa <jagaa1103@gmail.com>
2020-12-07 09:43:17 -0600

    start project

---
+++ EarthQuakeListApp/EarthQuakeListApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EarthQuakeListApp
+//
+//  Created by Enkhjargal Gansukh on 2020.12.07.
+//  Copyright © 2020 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        
+        return true
+    }
+
+
+}
+
---
+++ EarthQuakeListApp/EarthQuakeListAppTests/EarthQuakeListAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  EarthQuakeListAppTests.swift
+//  EarthQuakeListAppTests
+//
+//  Created by Enkhjargal Gansukh on 2020.12.07.
+//  Copyright © 2020 Enkhjargal Gansukh. All rights reserved.
+//
+
+import XCTest
+@testable import EarthQuakeListApp
+
+class EarthQuakeListAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
